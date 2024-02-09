76986ec707f505f46356c04a00b0a6380a0a5206
sam <samuel.hernandez.514@my.csun.edu>
2024-02-09 03:38:40 -0800

    Fix: Restructured API string for future use.

--- api/src/middleware/weather.js
+++ api/src/middleware/weather.js
@@ -8,7 +8,12 @@
 
 
 const axios = require('axios');
-const initial_api = 'https://api.weather.gov/points/40.0853,-100.6746';
+//structured this way so later, we can replace point_x and point_y with User-provided coordinates.
+const point_x = 40.0853
+const point_y = -100.6746
+const weather_api = 'https://api.weather.gov/points/'
+const initial_api = weather_api + String(point_x) + ',' + String(point_y);
+
 let forecast_api = '';
 
 axios.get(initial_api)
@@ -18,7 +23,6 @@ axios.get(initial_api)
     })
     .then(function(forecast_response){
         let forecast = forecast_response.data.properties.periods;
-
         // at this point we are inside the periods section of the forecast api
         // for example: https://api.weather.gov/gridpoints/GLD/93,85/forecast
         //uncomment the for loop to test what to retrieve for weather widget
@@ -26,7 +30,7 @@ axios.get(initial_api)
         // for (let i = 0; i < forecast.length; i++) {
         //     console.log(forecast[i].name);
         // }
-        
+
     })
     .catch(function (error){
         console.log(error);
