dba6af90d7906b129013392ebc32deacbd04e4e4
Mike Agazaryan <mikeagazarian@gmail.com>
2024-03-18 23:23:25 -0700

    Finished components design and icons
    Added component design for square boxes and icons within.

--- Citizen-Science/src/screens/ResourceScreen.tsx
+++ Citizen-Science/src/screens/ResourceScreen.tsx
@@ -1,63 +1,125 @@
 import { mdiStairsDown } from '@mdi/js';
 import React from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
-import {Avatar, Surface} from 'react-native-paper';
+import { View, StyleSheet, ImageBackground, SafeAreaViewComponent, Button } from 'react-native';
+import {Avatar, Surface, Icon, IconButton, Text} from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ResourceScreen = () => {
 
-    const image = require('../../assets/splashh.png');
+    const image = require('../../assets/csun.png');
 
     return (
         <SafeAreaView>
-         <View style={styles.container}>
-            {/* <View style={styles.avatarLeft }>
-                <Avatar.Image size={100} source={('/csun.png')} />
-            </View>
-            <View style={styles.avatarRight}>
-                <Avatar.Image size={100} source={('/csun.png')} />
-            </View> } */}
-
-            <Surface style={styles.surface} elevation={4}>
-                <Avatar.Image style={styles.avatarLeft} size={100} source={('Citizen-Science/assets/splashh.png')} />
-            </Surface>
             
+            <View style={styles.containerLeft}>
+            
+                <Surface style={styles.surfaceLeft} elevation={4}>
+                    <IconButton style={styles.avatarLeft} icon={'home'}></IconButton>
+                    
+                </Surface>
+                <View>
+                <Text style={styles.text} variant="displayLarge">Display Large</Text>
+                </View>
+                
+                
 
-        </View>
+                <Surface style={styles.surfaceLeft} elevation={4}>
+                    <IconButton style={styles.avatarLeft} icon={'beaker'}></IconButton>
+                </Surface>
+
+                <Surface style={styles.surfaceLeft} elevation={4}>
+                    <IconButton style={styles.avatarLeft} icon={'briefcase'}></IconButton>
+                </Surface>
+
+            </View>
+            <View style={styles.containerRight}>
+                <Surface style={styles.surfaceRight} elevation={4}>
+                    <IconButton style={styles.avatarLeft} icon={'human-male-female-child'}></IconButton>
+                </Surface>
+
+                <Surface style={styles.surfaceRight} elevation={4}>
+                    <IconButton style={styles.avatarLeft} icon={'tent'}></IconButton>
+                </Surface>
+
+                <Surface style={styles.surfaceRight} elevation={4}>
+                    <IconButton style={styles.avatarLeft} icon={'gavel'}></IconButton>
+                </Surface>
+
+            </View>
+            
         </SafeAreaView>
 
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
+    containerLeft: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        marginTop: 200,
+    },
+    containerRight: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 0,
     },
     text: {
-        fontSize: 28,
-        color: '#232f46',
+        
+        fontSize: 24,
+        color: '#000',
+                alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 200,
+        marginRight: 300,
+        paddingTop: 150,
+        paddingLeft: 150,
     },
     avatarLeft: {
-        backgroundColor: 'white',
+       backgroundColor: 'white',
        marginRight: 150,
        marginBottom: 150,
     },
     avatarRight: {
+        backgroundColor: 'white',
         
-        paddingLeft: 200,
+        marginLeft: 150,
+        marginBottom: 150,
        
     },
-    surface: {
+      surfaceRight: {
+        flex: 1,
         backgroundColor: 'white',
-        padding: 10,
+        height: 80,
+        width: 80,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 200,
+        marginLeft: 100,
+        paddingTop: 150,
+        paddingLeft: 150,
+      },
+      surfaceLeft: {
+        flex: 1,
+        backgroundColor: 'white',
         height: 80,
         width: 80,
         alignItems: 'center',
         justifyContent: 'center',
         marginTop: 200,
         marginRight: 300,
+        paddingTop: 150,
+        paddingLeft: 150,
+      },
+      surfaceight: {
+        backgroundColor: 'white',
+        height: 80,
+        width: 80,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 200,
+        marginLeft: 100,
         paddingTop: 150,
         paddingLeft: 150,
       },
