8da1b2122747dbf2446b246dc179a1a013d8458f
syoong27 <145421559+syoong81@users.noreply.github.com>
2024-04-19 14:49:14 -0700

    Map Page
    Map page containing added page and objects to:
    
    1) Add new gps pin
    2) Access device gps and autofill coordinates
    3) Allow user to upload images and read EXIF data (Still in progress)

---
+++ Citizen-Science/screens/AddPin.tsx
@@ -0,0 +1,278 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, StyleSheet, Text, TextInput, StatusBar, Alert } from 'react-native';
+import * as Location from 'expo-location';
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
+import { Picker } from '@react-native-picker/picker';
+
+const App = () => {
+  const [longitude, setLongitude] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [locationName, setLocationName] = useState('');
+  const [locationDetails, setLocationDetails] = useState('');
+  const [pinType, setPinType] = useState('hazard'); // Sets the default pin type to hazard
+  const [image, setImage] = useState(null);
+
+  //First checks for camera and GPS permissions and requests them as needed
+  useEffect(() => {
+    requestCameraPermission(); 
+    requestLocationPermission();
+  }, []);
+
+  //request for camera permission
+  const requestCameraPermission = async () => {
+    try {
+      
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access camera roll was denied');
+        return false;
+      }
+      return true;
+    } catch (err) {
+      console.warn(err);
+      return false;
+    }
+  }; 
+
+  //Request for location permission
+  const requestLocationPermission = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const getLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status === 'granted') {
+        const location = await Location.getCurrentPositionAsync({});
+        setLongitude(location.coords.longitude.toFixed(6));
+        setLatitude(location.coords.latitude.toFixed(6));
+      } else {
+        console.log('Permission to access location was denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+//Requests permissiono for Gallery
+  const pickImage = async () => {
+    const { status } = await MediaLibrary.getPermissionsAsync();
+    if (status !== 'granted') {
+      const { status: newStatus } = await MediaLibrary.requestPermissionsAsync();
+      if (newStatus !== 'granted') {
+        alert('Permission to access media library is required!');
+        return;
+      }
+    }
+  
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: false,
+      aspect: [4, 3],
+      quality: 1,
+      exif: true, //Enables access to EXIF data
+      base64: true, // Load the encoded base64 entire image
+    });
+  
+    if (!result.cancelled) {
+      const imageBase64 = result.assets[0].base64; // Access the base64-encoded image data
+  
+      console.log("Self Image Base64 ", imageBase64);
+  
+      // Convert base64-encoded image data to binary data
+      const binaryImageData = Buffer.from(imageBase64, 'base64');
+  
+      // Now you can process the binaryImageData, including extracting EXIF data if available
+      const exifData = extractExifData(binaryImageData);
+   
+      setImage(imageBase64); // Alternatively, set the image data to the state or perform further processing
+  
+      // Note: Loading the entire image as base64-encoded data can increase memory usage and may not be suitable for large images.
+    }
+  };
+
+  // const pickImage = async () => {
+  //   const { status } = await MediaLibrary.getPermissionsAsync();
+  // if (status !== 'granted') {
+  //   const { status: newStatus } = await MediaLibrary.requestPermissionsAsync();
+  //   if (newStatus !== 'granted') {
+  //     alert('Permission to access media library is required!');
+  //     return;
+  //   }
+  // }
+
+  // let result = await ImagePicker.launchImageLibraryAsync({
+  //   mediaTypes: ImagePicker.MediaTypeOptions.All,
+  //   allowsEditing: false,
+  //   aspect: [4, 3],
+  //   quality: 1,
+  //   exif: true, // Enable EXIF data retrieval
+  // });
+
+  // if (!result.cancelled) {
+
+  //   const imageUri = result.assets[0].uri;; // Access the image URI directly
+  //   console.log("Self Image URI ", imageUri);
+
+  //   // Check if EXIF data is available in the result
+  //   // if (result.exif) {
+  //   //   console.log("EXIF Data:", result.exif);
+  //   // } else {
+  //   //   console.log("No EXIF data available.");
+  //   // }
+
+  //   const assetInfo = await MediaLibrary.getAssetInfoAsync(imageUri);
+  //    if (assetInfo) {
+  //      console.log("Media Library EXIF Data:", assetInfo.exif);
+  //    } else {
+  //      console.log("Failed to retrieve asset information.");
+  //   }
+  
+  //     setImage(imageUri);
+    
+  //   }
+  // };
+
+
+  // const pickImage = async () => {
+  //   const permissionGranted = await requestCameraPermission();
+  //   if (!permissionGranted) return;
+
+  //   let result = await ImagePicker.launchImageLibraryAsync({
+  //     mediaTypes: ImagePicker.MediaTypeOptions.Images,
+  //     allowsEditing: false,
+  //     aspect: [4, 3],
+  //     quality: 1,
+  //     exif:true,
+  //   });
+
+  //  // console.log(result.assets[0].uri);
+  //   console.log(result.exif);
+
+  //   if (!result.cancelled) {
+  //     setImage(result.assets[0].uri);
+  //     console.log(result.assets[0].uri);
+  //   }
+  // };
+
+  //Function meant to 
+  function extractExifData(binaryImageData: any) {
+    throw new Error('Function not implemented.');
+  }
+
+  //Screen layout (Image,Buttons and Piker)
+  return (
+    <View style={styles.container}>
+      <Text style={styles.item}>Create A New Pin</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Location Name"
+        onChangeText={text => setLocationName(text)}
+        value={locationName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Local Name"
+        onChangeText={text => setLocationName(text)}
+        value={locationName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Location Details"
+        onChangeText={text => setLocationDetails(text)}
+        value={locationDetails}
+      />
+      <Picker
+        style={[styles.input, styles.picker]}
+        selectedValue={pinType}
+        onValueChange={(itemValue, itemIndex) => setPinType(itemValue)}
+      >
+        <Picker.Item label="Hazard" value="hazard" />
+        <Picker.Item label="Event" value="event" />
+        <Picker.Item label="Tourist" value="tourist" />
+        <Picker.Item label="Educational" value="educational" />
+      </Picker>
+      <TextInput
+        style={styles.input}
+        placeholder="Longitude"
+        onChangeText={text => setLongitude(text)}
+        value={longitude}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Latitude"
+        onChangeText={text => setLatitude(text)}
+        value={latitude}
+      />
+      <Button
+        title="Get Location"
+        onPress={getLocation}
+        style={styles.button}
+      />
+      <Button 
+        title="Pick an image from camera roll" 
+        style={styles.button}
+        onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: '#fff',
+    padding: 8,
+  },
+  item: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginHorizontal: 20,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  picker: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  image: {
+    resizeMode: 'contain',
+    height:'100%',
+    maxHeight: 400,
+    width: '100%',
+    maxWidth:300,
+        
+  },
+});
+
